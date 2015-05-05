b17e0930e4100f10b427ca88a1d208ad78bbe986
Rodrigo Argumedo <rodrigo.argumedo@outlook.com>
2015-05-05 00:20:14 +0000

    Article controllers, views and model created

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article
+  include Mongoid::Document
+  field :title, type: String
+  field :text, type: String
+  has_many :comments, dependent: :destroy
+  
+  validates :title, presence: true, length: { minimum: 5 }
+end
